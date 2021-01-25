9d80dbf585fa23171cb649785fa3e092cbde6c76
Lucas Figueiredo Bof <lucasfbof@gmail.com>
2021-01-25 20:41:03 -0300

    Add Review entity and db seed

--- backend/src/main/java/com/devsuperior/movieflix/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -1,13 +1,17 @@
 package com.devsuperior.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -24,11 +28,16 @@ public class Movie implements Serializable {
 	private String subTitle;
 	private Integer year;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String synopsis;
 
 	@ManyToOne
 	@JoinColumn(name = "genre_id")
 	private Genre genre;
+
+	@OneToMany(mappedBy = "movie")
+	private List<Review> reviews = new ArrayList<>();
 
 	public Movie() {
 	}
@@ -97,6 +106,10 @@ public class Movie implements Serializable {
 
 	public void setGenre(Genre genre) {
 		this.genre = genre;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
 	}
 
 	@Override
